aff4377586ce3eed4d824d1b18def264c5af9756
zaid366 <raozaid24@gmail.com>
2025-12-07 00:20:09 +0500

    Opprater

--- data base.js
+++ data base.js
@@ -1,11 +1,26 @@
 let  isLoggedIn = "12 "
 let num =  23
-console.log(num);
+// console.log(num);
 
-let blooLeanIsLoogedIn = Boolean  (isLoggedIn)
-console.log(blooLeanIsLoogedIn);
+// let blooLeanIsLoogedIn = Boolean  (isLoggedIn)
+// console.log(blooLeanIsLoogedIn);
 
 
 // 1 => ture ; 0 => fals ;
 // " " => Fals ; 
 // " zaid " => True 
+
+
+let negNum = 4
+let negVlue = -negNum
+console.log(negVlue);
+ 
+
+// oppraater's
+console.log(2+2);// addition
+console.log(2-2);// subtraction
+console.log(2*2);// Multiplication
+console.log(2**2);// Power
+console.log(2/2);// division
+console.log(2%2);// remainder  
+
