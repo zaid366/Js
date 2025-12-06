2c3acb81996eb8a3309f208195c4ef72b4276635
zaid366 <raozaid24@gmail.com>
2025-12-07 00:43:34 +0500

    More Code

--- Comparision.js
+++ Comparision.js
@@ -9,4 +9,11 @@ console.log("z" > 1);
 console.log("2" > 1);
 
 
+// comparision with null
+console.log(null > 1);
+console.log(null == 0);
+console.log(null < 0);
+// soome time conver to NAN,0,different erry time
+
+// avoid this type of code 
 
