db9bb56ceff7d0bc6312be17e8252361c4c60ab1
zaid366 <raozaid24@gmail.com>
2025-12-06 18:28:49 +0500

    More converson with investigation

--- data base.js
+++ data base.js
@@ -1,21 +1,10 @@
-let age =   23
-console.log(typeof(age));
+let  isLoggedIn = "23"
+let blooLeanIsLoogedIn = Boolean  (isLoggedIn)
+console.log(blooLeanIsLoogedIn);
+console.log(typeof(isLoggedIn));
 
-// number wits strin ?
-let strNum =   "33abc"
-console.log(typeof(strNum));
 
-// strinng convert to number
 
-let valuInNumer = Number (strNum)
-console.log(typeof(valuInNumer));
-
-// check if it's actuly a number 
-
-console.log(valuInNumer);
-
-// what if it's null ?
-let check = null
-console.log(typeof(check));
-console.log(check);
-
+// 1 => ture ; 0 => fals ;
+// " " => Fals ; 
+// " zaid " => True ;
