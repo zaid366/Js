10530afa4db049ceec0576d577ccafe0ba7243aa
zaid366 <raozaid24@gmail.com>
2025-12-08 20:35:37 +0500

    data type summary

--- Comparision.js
+++ Comparision.js
@@ -13,7 +13,10 @@ console.log("2" > 1);
 console.log(null > 1);
 console.log(null == 0);
 console.log(null < 0);
+
 // soome time conver to NAN,0,different erry time
 
 // avoid this type of code 
+
+
 
---
+++ data  type note.js
@@ -0,0 +1,26 @@
+//  primitiver 
+//  7 typye's  : stringe , number ,Boolean , null, Undeined ,symbols, bigInt
+
+const  score  =  100
+const  scoreValue =  100.3
+const  isLOggedIN  =  true
+const  temp  =  null
+let  userEmail; //it is undefined
+
+const   id = Symbol('123')
+const   id2 = Symbol('123')
+
+const bigNumber = 122231231
+// NON primitive 
+// arry 
+const heros = [ "supaer man","Iron man "," Thor"]; // arry
+
+
+//,objects,
+
+let  obj = {
+    name: "zadi",
+    age : 19,
+}
+
+// Funtion 
