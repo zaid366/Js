ddcbbd469eca5461497439855f8e9e51f94e958a
zaid366 <raozaid24@gmail.com>
2025-12-10 23:55:07 +0500

    striingss

---
+++ strings.js
@@ -0,0 +1,13 @@
+// Strings
+ let hello = "hello "+  " world "
+//  old sintex
+let name= " zaid "
+let age =  50
+console.log(name = age = "hi");
+
+// back  tack (``)
+
+console.log(`  helllo my name is ${name} My age is  ${age }`);
+
+// another way off declearingg stringe 
+const gameName = new String ("zaid"); 
