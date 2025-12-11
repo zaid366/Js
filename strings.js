0e34e8d23ee39915e6e557e600439dbd9fbb6e63
zaid366 <raozaid24@gmail.com>
2025-12-11 23:02:44 +0500

    more strings methods

--- strings.js
+++ strings.js
@@ -10,4 +10,23 @@ console.log(name = age = "hi");
 console.log(`  helllo my name is ${name} My age is  ${age }`);
 
 // another way off declearingg stringe 
-const gameName = new String ("zaid"); 
+const gameName = new String ("zaid-zoro"); 
+
+console.log(gameName.length);// show string leangth
+console.log(gameName.charAt(2));// shows the position of chrachter
+console.log(gameName.indexOf('i'));//show the chaeer on whta position 
+console.log(gameName.substring(0,4));//  print onlly the 0 to  3 potion
+
+// trim 
+const newStr = " zaid "
+console.log(newStr);
+console.log(newStr.trim());
+
+// replace 
+const url = "https.//zaid.coom/scs%az"
+console.log(url.replace('%az', 'name'))
+console.log(url);
+
+
+
+// and moore etc.....
